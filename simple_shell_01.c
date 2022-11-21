e56db690e6f6333bf7c5896bdd81ecfee3c3a5b6
tykoon787 <tykoon787@protonmail.com>
2022-11-22 00:10:20 +0300

    Updated code

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -7,15 +7,18 @@
 
 #define BUFFSIZE 1024
 
+/** 
+ * main- Entry Point
+ * simple_shell- A simulation of the shell
+ * Return: Always (0) on Success
+ * 
+ */
 int main(void)
 {
-	char *buffer, *cmd; 
-	size_t size = 40; 
-	char pathname[BUFFSIZE];
-	int status, read = 0; 
+	char *buffer, *cmd, pathname[BUFFSIZE];
+	size_t size = 40;
+	int read = 0;
 	pid_t child_proc;
-	pid_t parent_proc;
-
 
 	buffer = malloc(sizeof(char) * BUFFSIZE);
 
@@ -23,12 +26,12 @@ int main(void)
 	{
 		/* Reset pathname and cmd */
 		strcpy(pathname, "/bin/");
-		cmd = "";	
+		cmd = "";
 
 		/* Start Shell */
 		printf("$: ");
 		read = getline(&buffer, &size, stdin);
-		buffer[read - 1] = '\0'; 
+		buffer[read - 1] = '\0';
 		cmd = strcat(pathname, buffer);
 
 		char *argv[] = {cmd, NULL, NULL};
@@ -42,24 +45,25 @@ int main(void)
 			perror("Error: Cannot Run Command.\n");
 			exit(1);
 		}
-
+		
 		/* If child process is successful, execute command */
 		if (child_proc == 0)
 		{
-			if(execve(argv[0], argv, NULL) == -1)
+			if (execve(argv[0], argv, NULL) == -1)
 			{
 				printf("Command Not Found\n");
 				exit(1);
 			}
+
 		}
 
 		/* Return to parent process */
 		else
 		{
 			wait(NULL);
-			if (strcmp(buffer, "exit") == 0)
+			if (strcmp(argv[0], "/bin/exit") == 0)
 			{
-				printf("User Terminated\n");
+				printf("User Terminated\n");	
 				exit(1);
 			}
 		}
