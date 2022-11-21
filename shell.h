5c725dc853ae96dc9de7921dd1e54eee07271e65
tykoon787 <tykoon787@protonmail.com>
2022-11-22 00:31:17 +0300

    Created Headerfile

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFSIZE 1024
+
+#endif /* SHELL_H */
