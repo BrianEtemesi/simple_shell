671ea0c595bacb2ed059b259c049493eee102817
tykoon787 <tykoon787@protonmail.com>
2022-11-25 03:01:52 +0300

    Added Version 3 of print_shell project. 100%

---
+++ ver3/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "funcs.h"
+#include "prototypes.h"
+
+int main(void)
+{
+	char *buffer;
+	char **args, **args_cpy;
+	int status = 0, count = 10, i = 0;
+
+	do 
+	{
+		/* Print a promt */
+		printf("$: ");
+		
+		/* Read Line and store in buffer */
+		buffer = read_line();
+		printf("Buffer received: [%s]\n", buffer);
+
+		/* Tokenize */
+		args = tokenize(buffer);
+		
+		/* Copy pointer address of initial args */
+		args_cpy = args;
+
+		/* Print Every Argument */
+		while (*args)
+		{
+		printf("Args: [%s]\n", *args);
+		args++;
+		}
+
+		/* Reset args pointer back to original */
+		args = args_cpy;
+
+		status = proc_exe(args);
+		
+	} while (status);
+
+	return(0);
+}
