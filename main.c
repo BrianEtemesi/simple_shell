b678d09b846b3d63433294aae295d68afda924fb
tykoon787 <tykoon787@protonmail.com>
2022-11-25 03:32:25 +0300

    Cleaned up code by removing debug statements

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "funcs.h"
 #include "prototypes.h"
 
@@ -10,33 +9,13 @@
 int main(void)
 {
 	char *buffer;
-	char **args, **args_cpy;
+	char **args;
 	int status = 0;
 
 	do {
-		/* Print a promt */
 		printf("$: ");
-
-		/* Read Line and store in buffer */
 		buffer = read_line();
-		printf("Buffer received: [%s]\n", buffer);
-
-		/* Tokenize */
 		args = tokenize(buffer);
-
-		/* Copy pointer address of initial args */
-		args_cpy = args;
-
-		/* Print Every Argument */
-		while (*args)
-		{
-		printf("Args: [%s]\n", *args);
-		args++;
-		}
-
-		/* Reset args pointer back to original */
-		args = args_cpy;
-
 		status = proc_exe(args);
 
 	} while (status);
